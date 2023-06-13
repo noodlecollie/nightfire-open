f72aa3a9ecc5668efe9b6462ad1e7093004ad5eb
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-06-13 08:25:50 +0100

    Added vsnprintf replacement

--- platformlib/include/PlatformLib/String.h
+++ platformlib/include/PlatformLib/String.h
@@ -2,16 +2,30 @@
 #define PLATFORMLIB_STRING_H
 
 #include <stdlib.h>
+#include <stdarg.h>
 
 #ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
+extern "C"
+{
+#endif  // __cplusplus
 
-int PlatformLib_StrCaseCmp(const char *s1, const char *s2);
-int PlatformLib_StrNCaseCmp(const char *s1, const char *s2, size_t n);
+int PlatformLib_StrCaseCmp(const char* s1, const char* s2);
+int PlatformLib_StrNCaseCmp(const char* s1, const char* s2, size_t n);
+int PlatformLib_VSNPrintF(char* buffer, size_t count, const char* format, va_list argptr);
+
+static int PlatformLib_SNPrintF(char* buffer, size_t count, const char* format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	const int returnVal = PlatformLib_VSNPrintF(buffer, count, format, args);
+	va_end(args);
+
+	return returnVal;
+}
 
 #ifdef __cplusplus
-} // extern "C"
-#endif // __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
 
-#endif // PLATFORMLIB_STRING_H
+#endif  // PLATFORMLIB_STRING_H
--- platformlib/src/Win32/String.cpp
+++ platformlib/src/Win32/String.cpp
@@ -1,20 +1,30 @@
 #include "PlatformLib/String.h"
 #include <string.h>
+#include <stdio.h>
 
 #ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
+extern "C"
+{
+#endif  // __cplusplus
 
-int  PlatformLib_StrCaseCmp(const char *s1, const char *s2)
+int PlatformLib_StrCaseCmp(const char* s1, const char* s2)
 {
 	return _stricmp(s1, s2);
 }
 
-int PlatformLib_StrNCaseCmp(const char *s1, const char *s2, size_t n)
+int PlatformLib_StrNCaseCmp(const char* s1, const char* s2, size_t n)
 {
 	return _strnicmp(s1, s2, n);
 }
 
+// https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/vsnprintf-vsnprintf-vsnprintf-l-vsnwprintf-vsnwprintf-l
+// "The vsnprintf function always writes a null terminator, even if it truncates the output."
+// This makes this function safe for our use.
+int PlatformLib_VSNPrintF(char* buffer, size_t count, const char* format, va_list argptr)
+{
+	return vsnprintf(buffer, count, format, argptr);
+}
+
 #ifdef __cplusplus
-} // extern "C"
-#endif // __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
