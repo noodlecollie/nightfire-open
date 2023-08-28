89a080a6012c9f897448f61ea30811cc2d3b3819
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-08-28 18:40:40 +0100

    Created a script for compiling a player model

--- game/assets/models/player/compilePlayer.py
+++ game/assets/models/player/compilePlayer.py
@@ -9,10 +9,9 @@ STUDIOMDL_PATH = os.path.abspath(os.path.join(SCRIPT_DIR, "..", "..", "..", ".."
 MODEL_OUTPUT_ROOT_PATH = os.path.abspath(os.path.join(SCRIPT_DIR, "..", "..", "..", "content", "nfopen", "models"))
 
 QC_COMMAND_MODELNAME = "$modelname"
-EXPECTED_MODELNAME_PREFIXES = [ "p_", "v_", "w_", "w_ammo_" ]
 
 def parseArguments():
-	parser = argparse.ArgumentParser(description="Compiles the given weapons and copies the models to the game content folder.")
+	parser = argparse.ArgumentParser(description="Compiles the given player models and copies tuem to the game content folder.")
 
 	parser.add_argument("dirs",
 						nargs="+",
@@ -24,21 +23,7 @@ def ensureExists(dirPath : str):
 	if not os.path.isdir(dirPath):
 		raise OSError(f"Directory {dirPath} does not exist.")
 
-def modelNameWithoutPrefix(modelName : str):
-	modelName = os.path.splitext(modelName)[0]
-	longestPrefix = None
-
-	for prefix in EXPECTED_MODELNAME_PREFIXES:
-		if modelName.startswith(prefix):
-			longestPrefix = prefix
-
-	if longestPrefix is not None:
-		return modelName[len(longestPrefix):]
-	else:
-		raise ValueError(f"Model name '{modelName}' was not in expected format. " +
-						 f"Expected name prefix to be one of: {', '.join(EXPECTED_MODELNAME_PREFIXES)}.")
-
-def fetchWeaponModelNameFromQC(qcFilePath : str):
+def fetchModelNameFromQC(qcFilePath : str):
 	with open(qcFilePath, "r") as inFile:
 		lines = inFile.readlines()
 
@@ -53,14 +38,14 @@ def fetchWeaponModelNameFromQC(qcFilePath : str):
 
 	raise ValueError(f"{qcFilePath}: Could not find a {QC_COMMAND_MODELNAME} line within this file.")
 
-def outputDirForWeapon(weaponName : str):
-	return os.path.join(MODEL_OUTPUT_ROOT_PATH, f"weapon_{weaponName}")
+def outputDirForModel(modelName : str):
+	return os.path.join(MODEL_OUTPUT_ROOT_PATH, "player", os.path.splitext(modelName)[0])
 
 def runStudioMDL(qcFilePath : str):
 	print("Running StudioMDL for", qcFilePath)
 
 	print()
-	ret = subprocess.run([STUDIOMDL_PATH, qcFilePath], cwd=os.path.dirname(qcFilePath))
+	ret = subprocess.run([STUDIOMDL_PATH, "-e", qcFilePath], cwd=os.path.dirname(qcFilePath))
 	print()
 
 	if ret.returncode != 0:
@@ -72,33 +57,31 @@ def compileAndCopyToOutput(qcFilePath : list):
 	print("Found QC file:", qcFilePath)
 
 	localDir = os.path.dirname(qcFilePath)
-	weaponModelName = fetchWeaponModelNameFromQC(qcFilePath)
-	weaponName = modelNameWithoutPrefix(weaponModelName)
-	outputDir = outputDirForWeapon(weaponName)
+	modelName = fetchModelNameFromQC(qcFilePath)
+	outputDir = outputDirForModel(modelName)
 
-	print("  Weapon model name:", weaponModelName)
-	print("  Weapon name:", weaponName)
+	print("  Player model name:", modelName)
 	print("  Output directory:", outputDir)
 
 	runStudioMDL(qcFilePath)
 
 	# We expect the compiled model to be output to:
-	localWeaponModelPath = os.path.join(localDir, weaponModelName)
+	localModelPath = os.path.join(localDir, modelName)
 
-	if not os.path.isfile(localWeaponModelPath):
-		raise OSError(f"Could not find compiled model {localWeaponModelPath}")
+	if not os.path.isfile(localModelPath):
+		raise OSError(f"Could not find compiled model {localModelPath}")
 
-	targetWeaponModelPath = os.path.join(outputDir, weaponModelName)
+	targetModelPath = os.path.join(outputDir, modelName)
 
-	print("Copying compiled weapon model to", targetWeaponModelPath)
+	print("Copying compiled player model to", targetModelPath)
 
 	if not os.path.isdir(outputDir):
 		os.makedirs(outputDir)
 
-	shutil.copyfile(localWeaponModelPath, targetWeaponModelPath)
+	shutil.copyfile(localModelPath, targetModelPath)
 
-	print("Deleting local compiled model", localWeaponModelPath)
-	os.remove(localWeaponModelPath)
+	print("Deleting local compiled model", localModelPath)
+	os.remove(localModelPath)
 
 	print("Complete.")
 	print()
@@ -129,7 +112,7 @@ def main():
 
 	for subdir in args.dirs:
 		try:
-			print("Compiling weapon models in subdirectory:", subdir)
+			print("Compiling player models in subdirectory:", subdir)
 			compileRecursive(os.path.join(SCRIPT_DIR, subdir))
 			successfulDirs += 1
 		except Exception as ex:
