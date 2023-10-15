517e8aa7658135ee8a48022acc2d0182f8302d0b
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-10-15 16:41:56 +0100

    Created buffered file reader class

---
+++ tools/mdl-decompiler/cpp/src/BufferedFileReader.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <type_traits>
+#include <functional>
+
+class BufferedFileReader
+{
+public:
+	explicit BufferedFileReader(const std::string& filePath);
+
+	size_t CurrentPosition() const;
+	bool PositionIsEOF() const;
+	void Seek(int64_t delta);
+	void SeekForward(size_t delta);
+	void SeekBackward(size_t delta);
+	void SeekFromBeginning(size_t offset);
+	void SeekFromEnd(size_t offset);
+	void GoToBeginning();
+	void GoToEnd();
+
+	void ReadBytes(void* buffer, size_t length);
+
+	template<typename TargetType, typename ContainerType = TargetType, typename ConverterFunctor>
+	TargetType ReadElement(const ConverterFunctor& converter)
+	{
+		static_assert(
+			std::is_trivial<ContainerType>::value,
+			"Container type must be trivial for a safe memcpy to take place.");
+
+		ContainerType container;
+		ReadBytes(&container, sizeof(container));
+
+		return converter(container);
+	}
+
+	template<typename TargetType, typename ContainerType = TargetType, typename ConverterFunctor>
+	std::vector<TargetType> ReadElements(size_t numElements, const ConverterFunctor& converter)
+	{
+		static_assert(
+			std::is_trivial<ContainerType>::value,
+			"Container type must be trivial for a safe memcpy to take place.");
+
+		if ( numElements < 1 )
+		{
+			return std::vector<TargetType>();
+		}
+
+		std::vector<ContainerType> intermediateElements(numElements);
+		ReadBytes(intermediateElements.data(), intermediateElements.size() * sizeof(ContainerType));
+
+		std::vector<TargetType> outVector;
+		outVector.reserve(intermediateElements.size());
+
+		for ( size_t index = 0; index < intermediateElements.size(); ++index )
+		{
+			outVector.emplace_back(converter(index, intermediateElements[index]));
+		}
+
+		return outVector;
+	}
+
+	// Common use specialisations:
+
+	template<typename TargetType, typename ContainerType = TargetType>
+	TargetType ReadElement()
+	{
+		return ReadElement<TargetType, ContainerType, std::function<TargetType(const ContainerType&)>>(
+			[](const ContainerType& in) -> TargetType
+			{
+				return TargetType(in);
+			});
+	}
+
+	template<typename TargetType>
+	TargetType ReadElement()
+	{
+		static_assert(
+			std::is_trivial<TargetType>::value,
+			"Target type must be trivial for a safe memcpy to take place.");
+
+		TargetType out;
+		ReadBytes(&out, sizeof(out));
+
+		return out;
+	}
+
+	template<typename TargetType, typename ContainerType = TargetType>
+	std::vector<TargetType> ReadElements(size_t numElements)
+	{
+		return ReadElements<TargetType, ContainerType, std::function<TargetType(size_t, const ContainerType&)>>(
+			[](size_t, const ContainerType& in)
+			{
+				return TargetType(in);
+			});
+	}
+
+	template<typename TargetType>
+	std::vector<TargetType> ReadElements(size_t numElements)
+	{
+		static_assert(
+			std::is_trivial<TargetType>::value,
+			"Target type must be trivial for a safe memcpy to take place.");
+
+		if ( !array )
+		{
+			throw FileIOException(m_FilePath, "Cannot read elements into null buffer.");
+		}
+
+		if ( numElements < 1 )
+		{
+			return std::vector<TargetType>();
+		}
+
+		std::vector<TargetType> outVector(numElements);
+		ReadBytes(outVector.data(), outVector.size() * sizeof(TargetType));
+
+		return outVector;
+	}
+
+private:
+	bool DeltaWouldExceedFile(size_t delta) const;
+
+	std::string m_FilePath;
+	std::vector<uint8_t> m_Data;
+	size_t m_CurrentPos = 0;
+};
---
+++ tools/mdl-decompiler/cpp/src/Exceptions.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+
+class BaseException : public std::exception
+{
+public:
+	const char* what() const noexcept override
+	{
+		if ( !m_BuiltErrorString )
+		{
+			m_ErrorString = BuildErrorString();
+			m_BuiltErrorString = true;
+		}
+
+		return m_ErrorString.c_str();
+	}
+
+protected:
+	virtual std::string BuildErrorString() const noexcept = 0;
+
+private:
+	mutable std::string m_ErrorString;
+	mutable bool m_BuiltErrorString = false;
+};
+
+class FileIOException : public BaseException
+{
+public:
+	explicit FileIOException(std::string filePath, std::string description) :
+		m_FilePath(std::move(filePath)),
+		m_Description(std::move(description))
+	{
+	}
+
+	std::string FilePath() const
+	{
+		return m_FilePath;
+	}
+
+	std::string Description() const
+	{
+		return m_Description;
+	}
+
+protected:
+	std::string BuildErrorString() const noexcept override
+	{
+		return m_FilePath + ": " + m_Description;
+	}
+
+private:
+	std::string m_FilePath;
+	std::string m_Description;
+};
