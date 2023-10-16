a04558b68c8b5aa41e897405bf55ab35c2986fd4
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-10-16 17:41:23 +0100

    Added decompiler argument parsing

--- tools/mdl-decompiler/cpp/src/Main.cpp
+++ tools/mdl-decompiler/cpp/src/Main.cpp
@@ -1,4 +1,45 @@
-int main(int, char**)
+#include <iostream>
+#include "args/args.hxx"
+
+int main(int argc, char** argv)
 {
+	args::ArgumentParser parser(EXECUTABLE_NAME " - Decompiles Nightfire v14 MDL files to SMD/QC files.");
+
+	parser.helpParams.showTerminator = false;
+
+	args::HelpFlag help(parser, "help", "Display this help menu.", {'h', "help"});
+	args::Positional<std::string> inputFileArg(parser, "input_file", "MDLv14 file to decompile.");
+
+	try
+	{
+		parser.ParseCLI(argc, argv);
+	}
+	catch ( const args::Completion& e )
+	{
+		std::cout << e.what();
+		return 1;
+	}
+	catch ( const args::Help& )
+	{
+		std::cout << parser;
+		return 1;
+	}
+	catch ( const args::ParseError& e )
+	{
+		std::cerr << e.what() << std::endl;
+		std::cerr << parser;
+		return 1;
+	}
+
+	if ( !inputFileArg )
+	{
+		std::cerr << "No input file was specified." << std::endl;
+		return 1;
+	}
+
+	// TODO
+	std::string inputFilePath = args::get(inputFileArg);
+	std::cout << "Decompiling: " << inputFilePath << std::endl;
+
 	return 0;
 }
