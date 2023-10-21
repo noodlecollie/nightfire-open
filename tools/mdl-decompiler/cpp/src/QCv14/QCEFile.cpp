c37de655d9dbc018a428d3cfc0fa4e21e3e3b745
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-10-21 11:09:45 +0100

    Added QCE file

---
+++ tools/mdl-decompiler/cpp/src/QCv14/QCEFile.cpp
@@ -0,0 +1,9 @@
+#include "QCv14/QCEFile.h"
+
+namespace QCv14
+{
+	void QCEFile::Write(std::ostream& /* stream */) const
+	{
+		// TODO
+	}
+}  // namespace QCv14
---
+++ tools/mdl-decompiler/cpp/src/QCv14/QCEFile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <ostream>
+
+namespace QCv14
+{
+	class QCEFile
+	{
+	public:
+		void Write(std::ostream& stream) const;
+	};
+}
