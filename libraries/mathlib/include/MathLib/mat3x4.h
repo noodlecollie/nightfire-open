7e6cfb67c75e622c7a9ea5eb43f3af23fab39bcd
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-12-26 22:26:22 +0000

    Added map.cpp

--- libraries/mathlib/include/MathLib/mat3x4.h
+++ libraries/mathlib/include/MathLib/mat3x4.h
@@ -19,6 +19,7 @@ void Matrix3x4_ConcatTransforms(matrix3x4 out, const matrix3x4 in1, const matrix
 void Matrix3x4_FromOriginQuat(matrix3x4 out, const quat_t quaternion, const vec3_t origin);
 void Matrix3x4_FromQuat(matrix3x4 out, const quat_t quaternion);
 void Matrix3x4_CreateFromEntity(matrix3x4 out, const vec3_t angles, const vec3_t origin, float scale);
+void Matrix3x4_CreateFromEntityScale3f(matrix3x4 out, const vec3_t angles, const vec3_t origin, const vec3_t scale);
 void Matrix3x4_TransformAABB(const matrix3x4 world, const vec3_t mins, const vec3_t maxs, vec3_t absmin, vec3_t absmax);
 void Matrix3x4_GetOrientationVectors(const matrix3x4 matrix, vec3_t forward, vec3_t right, vec3_t up);
 
@@ -29,6 +30,8 @@ void Matrix3x4_VectorTransform(const matrix3x4 in, const vec3_t v, vec3_t out);
 void Matrix3x4_VectorITransform(const matrix3x4 in, const vec3_t v, vec3_t out);
 void Matrix3x4_VectorRotate(const matrix3x4 in, const vec3_t v, vec3_t out);
 void Matrix3x4_VectorIRotate(const matrix3x4 in, const vec3_t v, vec3_t out);
+
+void Matrix3x4_MatrixToEntityScale3f(const matrix3x4 in, vec3_t origin, vec3_t angles, vec3_t scale);
 
 static inline void Matrix3x4_LoadIdentity(matrix3x4 mat)
 {
