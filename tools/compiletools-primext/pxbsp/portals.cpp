2257c14ecd527d3351bfe42bdf09784a057a88a4
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-12-27 10:17:27 +0000

    Added portals.cpp

--- tools/compiletools-primext/pxbsp/portals.cpp
+++ tools/compiletools-primext/pxbsp/portals.cpp
@@ -1,17 +1,19 @@
 /***
-*
-*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
-*
-****/
+ *
+ *	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+ *
+ *	This product contains software technology licensed from Id
+ *	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+ *	All Rights Reserved.
+ *
+ ****/
 
+#include "CRTLib/crtlib.h"
+#include "MathLib/utils.h"
+#include "CompileTools/conprint.h"
 #include "bsp5.h"
 
-
-node_t	g_outside_node;		// portals outside the world face this
+node_t g_outside_node;  // portals outside the world face this
 
 //=============================================================================
 /*
@@ -19,15 +21,15 @@ node_t	g_outside_node;		// portals outside the world face this
 AddPortalToNodes
 =============
 */
-void AddPortalToNodes( portal_t *p, node_t *front, node_t *back )
+void AddPortalToNodes(portal_t* p, node_t* front, node_t* back)
 {
-	if( p->nodes[0] || p->nodes[1] )
-		COM_FatalError( "AddPortalToNode: allready included\n" );
+	if ( p->nodes[0] || p->nodes[1] )
+		COM_FatalError("AddPortalToNode: allready included\n");
 
 	p->nodes[0] = front;
 	p->next[0] = front->portals;
 	front->portals = p;
-	
+
 	p->nodes[1] = back;
 	p->next[1] = back->portals;
 	back->portals = p;
@@ -38,44 +40,57 @@ void AddPortalToNodes( portal_t *p, node_t *front, node_t *back )
 RemovePortalFromNode
 =============
 */
-void RemovePortalFromNode( portal_t *portal, node_t *l )
+void RemovePortalFromNode(portal_t* portal, node_t* l)
 {
-	portal_t	**pp, *t;
-	
+	portal_t **pp, *t;
+
 	// remove reference to the current portal
 	pp = &l->portals;
-	while( 1 )
+	while ( 1 )
 	{
 		t = *pp;
-		if( !t ) COM_FatalError( "RemovePortalFromNode: portal not in leaf\n" );	
 
-		if( t == portal )
+		if ( !t )
+		{
+			COM_FatalError("RemovePortalFromNode: portal not in leaf\n");
+		}
+
+		if ( t == portal )
+		{
 			break;
+		}
 
-		if( t->nodes[0] == l )
+		if ( t->nodes[0] == l )
+		{
 			pp = &t->next[0];
-		else if( t->nodes[1] == l )
+		}
+		else if ( t->nodes[1] == l )
+		{
 			pp = &t->next[1];
-		else COM_FatalError( "RemovePortalFromNode: portal not bounding leaf\n" );
+		}
+		else
+		{
+			COM_FatalError("RemovePortalFromNode: portal not bounding leaf\n");
+		}
 	}
-	
-	if( portal->nodes[0] == l )
+
+	if ( portal->nodes[0] == l )
 	{
 		*pp = portal->next[0];
 		portal->nodes[0] = NULL;
 	}
-	else if( portal->nodes[1] == l )
+	else if ( portal->nodes[1] == l )
 	{
-		*pp = portal->next[1];	
+		*pp = portal->next[1];
 		portal->nodes[1] = NULL;
 	}
 }
 
 //============================================================================
 
-void PrintPortal( portal_t *p )
+void PrintPortal(portal_t* p)
 {
-	pw( p->winding );
+	pw(p->winding);
 }
 
 /*
@@ -85,34 +100,34 @@ MakeHeadnodePortals
 The created portals will face the global outside_node
 ================
 */
-void MakeHeadnodePortals( node_t *node, const vec3_t mins, const vec3_t maxs )
+void MakeHeadnodePortals(node_t* node, const vec3_t mins, const vec3_t maxs)
 {
-	vec3_t	bounds[2];
-	portal_t	*p, *portals[6];
-	plane_t	bplanes[6], *pl;
-	int	i, j, n;
+	vec3_t bounds[2];
+	portal_t *p, *portals[6];
+	plane_t bplanes[6], *pl;
+	int i, j, n;
 
 	// pad with some space so there will never be null volume leafs
-	VectorCopy( mins, bounds[0] );
-	VectorCopy( maxs, bounds[1] );
+	VectorCopy(mins, bounds[0]);
+	VectorCopy(maxs, bounds[1]);
 
-	ExpandBounds( bounds[0], bounds[1], SIDESPACE );
+	ExpandBounds(bounds[0], bounds[1], SIDESPACE);
 	g_outside_node.contents = CONTENTS_SOLID;
 	g_outside_node.portals = NULL;
 
-	for( i = 0; i < 3; i++ )
+	for ( i = 0; i < 3; i++ )
 	{
-		for( j = 0; j < 2; j++ )
+		for ( j = 0; j < 2; j++ )
 		{
 			n = j * 3 + i;
 
-			p = AllocPortal ();
+			p = AllocPortal();
 			portals[n] = p;
-			
-			pl = &bplanes[n];
-			memset( pl, 0, sizeof( *pl ));
 
-			if( j )
+			pl = &bplanes[n];
+			memset(pl, 0, sizeof(*pl));
+
+			if ( j )
 			{
 				pl->normal[i] = -1;
 				pl->dist = -bounds[j][i];
@@ -123,19 +138,20 @@ void MakeHeadnodePortals( node_t *node, const vec3_t mins, const vec3_t maxs )
 				pl->dist = bounds[j][i];
 			}
 
-			p->planenum = FindFloatPlane( pl->normal, pl->dist );
-			p->winding = BaseWindingForPlane( pl->normal, pl->dist );
-			AddPortalToNodes( p, node, &g_outside_node );
+			p->planenum = FindFloatPlane(pl->normal, pl->dist);
+			p->winding = BaseWindingForPlane(pl->normal, pl->dist);
+			AddPortalToNodes(p, node, &g_outside_node);
 		}
-	}		
+	}
 
 	// clip the basewindings by all the other planes
-	for( i = 0; i < 6; i++ )
+	for ( i = 0; i < 6; i++ )
 	{
-		for( j = 0; j < 6; j++ )
+		for ( j = 0; j < 6; j++ )
 		{
-			if( j == i ) continue;
-			ChopWindingInPlace( &portals[i]->winding, bplanes[j].normal, bplanes[j].dist, g_prtepsilon );
+			if ( j == i )
+				continue;
+			ChopWindingInPlace(&portals[i]->winding, bplanes[j].normal, bplanes[j].dist, g_prtepsilon);
 		}
 	}
 }
@@ -149,37 +165,37 @@ PortalSidesVisible
 translucent water support
 ================
 */
-bool PortalSidesVisible( portal_t *p )
+bool PortalSidesVisible(portal_t* p)
 {
 #if 1
-	int	contents0 = p->nodes[0]->contents;
-	int	contents1 = p->nodes[1]->contents;
+	int contents0 = p->nodes[0]->contents;
+	int contents1 = p->nodes[1]->contents;
 
 	// can't see through solids
-	if( contents0 == CONTENTS_SOLID || contents1 == CONTENTS_SOLID )
+	if ( contents0 == CONTENTS_SOLID || contents1 == CONTENTS_SOLID )
 		return false;
 
 	// if contents values are the same and not solid, can see through
-	if( contents0 == contents1 )
+	if ( contents0 == contents1 )
 		return true;
 
 	// can't see through func_illusionary_visblocker
-	if( contents0 == CONTENTS_VISBLOCKER || contents1 == CONTENTS_VISBLOCKER )
+	if ( contents0 == CONTENTS_VISBLOCKER || contents1 == CONTENTS_VISBLOCKER )
 		return false;
 
-	if( IsLiquidContents( contents0 ) && contents1 == CONTENTS_EMPTY )
+	if ( IsLiquidContents(contents0) && contents1 == CONTENTS_EMPTY )
 		return true;
 
-	if( IsLiquidContents( contents1 ) && contents0 == CONTENTS_EMPTY )
+	if ( IsLiquidContents(contents1) && contents0 == CONTENTS_EMPTY )
 		return true;
 
 	return false;
 #else
-	if( p->nodes[0]->contents == p->nodes[1]->contents )
+	if ( p->nodes[0]->contents == p->nodes[1]->contents )
 		return true;
 
-	if( p->nodes[0]->contents != CONTENTS_SOLID && p->nodes[1]->contents != CONTENTS_SOLID
-	 && p->nodes[0]->contents != CONTENTS_SKY && p->nodes[1]->contents != CONTENTS_SKY )
+	if ( p->nodes[0]->contents != CONTENTS_SOLID && p->nodes[1]->contents != CONTENTS_SOLID &&
+		 p->nodes[0]->contents != CONTENTS_SKY && p->nodes[1]->contents != CONTENTS_SKY )
 		return true;
 
 	return false;
@@ -194,90 +210,91 @@ PORTAL FILE GENERATION
 ==============================================================================
 */
 
-FILE	*pf;
-int	num_visleafs;	// leafs the player can be in
-int	num_visportals;
-int	c_backward_portals;
-int	c_colinear_portals;
+FILE* pf;
+int num_visleafs;  // leafs the player can be in
+int num_visportals;
+int c_backward_portals;
+int c_colinear_portals;
 
-static void WritePortalFile_r( node_t *node )
+static void WritePortalFile_r(node_t* node)
 {
-	plane_t	*plane, plane2;
-	portal_t	*p, *next;
-	winding_t	*w;
-	int	i;
+	plane_t *plane, plane2;
+	portal_t *p, *next;
+	winding_t* w;
+	int i;
 
-	if( !FBitSet( node->flags, FNODE_LEAFPORTAL ))
+	if ( !FBitSet(node->flags, FNODE_LEAFPORTAL) )
 	{
-		WritePortalFile_r( node->children[0] );
-		WritePortalFile_r( node->children[1] );
+		WritePortalFile_r(node->children[0]);
+		WritePortalFile_r(node->children[1]);
 		return;
 	}
-	
-	if( node->contents == CONTENTS_SOLID )
+
+	if ( node->contents == CONTENTS_SOLID )
 		return;
 
-	for( p = node->portals; p != NULL; p = next )
+	for ( p = node->portals; p != NULL; p = next )
 	{
 		next = (p->nodes[0] == node) ? p->next[0] : p->next[1];
 
-		if( !p->winding || p->nodes[0] != node )
+		if ( !p->winding || p->nodes[0] != node )
 			continue;
 
-		if( !PortalSidesVisible( p ))
+		if ( !PortalSidesVisible(p) )
 			continue;
 
 		w = p->winding;
 
 		// write out to the file
-			
+
 		// sometimes planes get turned around when they are very near
 		// the changeover point between different axis.  interpret the
 		// plane the same way vis will, and flip the side orders if needed
-		WindingPlane( w, plane2.normal, &plane2.dist );
+		WindingPlane(w, plane2.normal, &plane2.dist);
 		plane = &g_mapplanes[p->planenum];
 
-		if( DotProduct( plane->normal, plane2.normal ) < ( 1.0 - g_prtepsilon ))
+		if ( DotProduct(plane->normal, plane2.normal) < (1.0 - g_prtepsilon) )
 		{
-			if( DotProduct( plane->normal, plane2.normal ) > -1.0 + g_prtepsilon )
+			if ( DotProduct(plane->normal, plane2.normal) > -1.0 + g_prtepsilon )
 				c_colinear_portals++;
-			else c_backward_portals++;
+			else
+				c_backward_portals++;
 
-			fprintf( pf, "%i %i %i ", w->numpoints, p->nodes[1]->visleafnum, p->nodes[0]->visleafnum );
+			fprintf(pf, "%i %i %i ", w->numpoints, p->nodes[1]->visleafnum, p->nodes[0]->visleafnum);
 		}
 		else
 		{
-			fprintf( pf, "%i %i %i ", w->numpoints, p->nodes[0]->visleafnum, p->nodes[1]->visleafnum );
+			fprintf(pf, "%i %i %i ", w->numpoints, p->nodes[0]->visleafnum, p->nodes[1]->visleafnum);
 		}
 
-		for( i = 0; i < w->numpoints; i++ )
+		for ( i = 0; i < w->numpoints; i++ )
 		{
-			fprintf( pf, "(%f %f %f) ", w->p[i][0], w->p[i][1], w->p[i][2] );
+			fprintf(pf, "(%f %f %f) ", w->p[i][0], w->p[i][1], w->p[i][2]);
 		}
 
-		fprintf( pf,"\n" );
+		fprintf(pf, "\n");
 	}
 }
-	
+
 /*
 ================
 NumberLeafs_r
 ================
 */
-void NumberLeafs_r( node_t *node )
+void NumberLeafs_r(node_t* node)
 {
-	portal_t	*p;
+	portal_t* p;
 
-	if( !FBitSet( node->flags, FNODE_LEAFPORTAL ))
-	{	
+	if ( !FBitSet(node->flags, FNODE_LEAFPORTAL) )
+	{
 		// decision node
 		node->visleafnum = -99;
-		NumberLeafs_r( node->children[0] );
-		NumberLeafs_r( node->children[1] );
+		NumberLeafs_r(node->children[0]);
+		NumberLeafs_r(node->children[1]);
 		return;
 	}
 
-	if( node->contents == CONTENTS_SOLID )
+	if ( node->contents == CONTENTS_SOLID )
 	{
 		// solid block, viewpoint never inside
 		node->visleafnum = -1;
@@ -285,19 +302,19 @@ void NumberLeafs_r( node_t *node )
 	}
 
 	node->visleafnum = num_visleafs++;
-	
-	for( p = node->portals; p != NULL; )
+
+	for ( p = node->portals; p != NULL; )
 	{
-		if( p->nodes[0] == node )
+		if ( p->nodes[0] == node )
 		{
 			// only write out from first leaf
-			if( p->winding && PortalSidesVisible( p ))
+			if ( p->winding && PortalSidesVisible(p) )
 				num_visportals++;
 			p = p->next[0];
 		}
-		else p = p->next[1];		
+		else
+			p = p->next[1];
 	}
-
 }
 
 /*
@@ -305,11 +322,11 @@ void NumberLeafs_r( node_t *node )
 CountChildLeafs_r
 ================
 */
-static int CountChildLeafs_r( node_t *node )
+static int CountChildLeafs_r(node_t* node)
 {
-	if( node->planenum == PLANENUM_LEAF )
+	if ( node->planenum == PLANENUM_LEAF )
 	{
-		if( FBitSet( node->flags, FNODE_DETAILCONTENTS ))
+		if ( FBitSet(node->flags, FNODE_DETAILCONTENTS) )
 		{
 			// solid
 			return 0;
@@ -322,10 +339,10 @@ static int CountChildLeafs_r( node_t *node )
 	else
 	{
 		// node
-		int	count = 0;
+		int count = 0;
 
-		count += CountChildLeafs_r( node->children[0] );
-		count += CountChildLeafs_r( node->children[1] );
+		count += CountChildLeafs_r(node->children[0]);
+		count += CountChildLeafs_r(node->children[1]);
 
 		return count;
 	}
@@ -336,20 +353,20 @@ static int CountChildLeafs_r( node_t *node )
 WriteLeafCount_r
 ================
 */
-static void WriteLeafCount_r( node_t *node )
+static void WriteLeafCount_r(node_t* node)
 {
-	if( !FBitSet( node->flags, FNODE_LEAFPORTAL ))
+	if ( !FBitSet(node->flags, FNODE_LEAFPORTAL) )
 	{
-		WriteLeafCount_r( node->children[0] );
-		WriteLeafCount_r( node->children[1] );
+		WriteLeafCount_r(node->children[0]);
+		WriteLeafCount_r(node->children[1]);
 	}
 	else
 	{
-		if( node->contents == CONTENTS_SOLID )
+		if ( node->contents == CONTENTS_SOLID )
 			return;
 
-		int count = CountChildLeafs_r( node );
-		fprintf( pf, "%i\n", count );
+		int count = CountChildLeafs_r(node);
+		fprintf(pf, "%i\n", count);
 	}
 }
 
@@ -358,61 +375,69 @@ static void WriteLeafCount_r( node_t *node )
 WritePortalfile
 ================
 */
-void WritePortalfile( tree_t *tree, bool leaked )
+void WritePortalfile(tree_t* tree, bool leaked)
 {
-	char	shortname[1024];
+	char shortname[1024];
 
 	num_visleafs = 0;
 	num_visportals = 0;
 	c_backward_portals = 0;
 	c_colinear_portals = 0;
 
-	COM_FileBase( g_portfilename, shortname );
+	COM_FileBase(g_portfilename, shortname, sizeof(shortname));
 
 	// set the visleafnum field in every leaf
 	// and count the total number of portals
-	NumberLeafs_r( tree->headnode );
-	
+	NumberLeafs_r(tree->headnode);
+
 	// write the file
-	if( leaked ) MsgDev( D_INFO, "^3BSP probably has a leak, writing portal file:^7 %s.prt\n", shortname );
-	else MsgDev( D_INFO, "^2BSP generation successful, writing portal file:^7 %s.prt\n", shortname );
-	pf = fopen( g_portfilename, "w" );
-	if( !pf ) COM_FatalError( "error opening %s\n", g_portfilename );
+	if ( leaked )
+	{
+		MsgDev(D_INFO, "^3BSP probably has a leak, writing portal file:^7 %s.prt\n", shortname);
+	}
+	else
+	{
+		MsgDev(D_INFO, "^2BSP generation successful, writing portal file:^7 %s.prt\n", shortname);
+	}
 
-	fprintf( pf, "%i\n", num_visleafs );
-	fprintf( pf, "%i\n", num_visportals );
+	pf = fopen(g_portfilename, "w");
+	if ( !pf )
+		COM_FatalError("error opening %s\n", g_portfilename);
 
-	WriteLeafCount_r( tree->headnode );
-	WritePortalFile_r( tree->headnode );
-	
-	fclose( pf );
+	fprintf(pf, "%i\n", num_visleafs);
+	fprintf(pf, "%i\n", num_visportals);
 
-	MsgDev( D_REPORT, "%i colinear portals\n", c_colinear_portals );
-	MsgDev( D_REPORT, "%i backward portals\n", c_backward_portals );
+	WriteLeafCount_r(tree->headnode);
+	WritePortalFile_r(tree->headnode);
+
+	fclose(pf);
+
+	MsgDev(D_REPORT, "%i colinear portals\n", c_colinear_portals);
+	MsgDev(D_REPORT, "%i backward portals\n", c_backward_portals);
 }
 
-
 //===================================================
-void FreePortals( node_t *node )
+void FreePortals(node_t* node)
 {
-	portal_t	*p, *nextp;
+	portal_t *p, *nextp;
 
-	if( node->children[0] )
-		FreePortals( node->children[0] );
+	if ( node->children[0] )
+		FreePortals(node->children[0]);
 
-	if( node->children[1] )
-		FreePortals( node->children[1] );
+	if ( node->children[1] )
+		FreePortals(node->children[1]);
 
-	for( p = node->portals; p != NULL; p = nextp )
+	for ( p = node->portals; p != NULL; p = nextp )
 	{
-		if( p->nodes[0] == node )
+		if ( p->nodes[0] == node )
 			nextp = p->next[0];
-		else nextp = p->next[1];
+		else
+			nextp = p->next[1];
 
-		RemovePortalFromNode( p, p->nodes[0] );
-		RemovePortalFromNode( p, p->nodes[1] );
-		FreeWinding( p->winding );
-		FreePortal( p );
+		RemovePortalFromNode(p, p->nodes[0]);
+		RemovePortalFromNode(p, p->nodes[1]);
+		FreeWinding(p->winding);
+		FreePortal(p);
 	}
 }
 
@@ -421,7 +446,7 @@ void FreePortals( node_t *node )
 FreeTreePortals
 ==================
 */
-void FreeTreePortals( tree_t *tree )
+void FreeTreePortals(tree_t* tree)
 {
-	FreePortals( tree->headnode );
+	FreePortals(tree->headnode);
 }
