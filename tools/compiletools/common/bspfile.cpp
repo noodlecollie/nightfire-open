bc59b2968ac1b810387d07886fae9a251581be92
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-12-29 22:34:20 +0000

    Got client-side models to work!

--- tools/compiletools/common/bspfile.cpp
+++ tools/compiletools/common/bspfile.cpp
@@ -1981,7 +1981,12 @@ void MakeClientEntity_Model(const entity_t& entity)
 	VectorCopy(entity.origin, outModel->origin);
 	safe_strncpy(outModel->model, ValueForKey(&entity, "model"), sizeof(outModel->model));
 
-	if ( sscanf(ValueForKey(&entity, "angles"), "%f %f %f", &outModel->angles[0], &outModel->angles[1], &outModel->angles[2]) != 3 )
+	if ( sscanf(ValueForKey(&entity, "angles"), "%f %f %f", &outModel->angles[0], &outModel->angles[1], &outModel->angles[2]) == 3 )
+	{
+		// Invert pitch, so that the model points in the expected direction in the engine.
+		outModel->angles[0] *= -1.0f;
+	}
+	else
 	{
 		VectorClear(outModel->angles);
 	}
---
+++ xash3d_engine/engine/src/client/cl_cliententities.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "EnginePublicAPI/com_model.h"
+
+void ClientEntities_LoadForBSP(model_t* model);
