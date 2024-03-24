9fa9f2c43e6ed760683fc4535453f89aa8c18f87
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-03-24 11:48:30 +0000

    Added Nuklear UI library

---
+++ game/game_libs/ui/nuklear_impl/nuklear_ui.c
@@ -0,0 +1,2 @@
+#define NK_IMPLEMENTATION
+#include "nuklear_impl/nuklear_ui.h"
---
+++ game/game_libs/ui/nuklear_impl/nuklear_ui.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+
+#include "nuklear/nuklear.h"
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic pop
+#endif
