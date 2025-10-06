73766aaa4b3f7776f65c04cc60ec43bbb0a35594
Vesper <3692034+noodlecollie@users.noreply.github.com>
2025-10-06 10:45:35 +0100

    Added function stubs

--- game/game_libs/ui_new/src/udll_int.cpp
+++ game/game_libs/ui_new/src/udll_int.cpp
@@ -5,38 +5,179 @@ ui_enginefuncs_t gEngfuncs;
 ui_extendedfuncs_t gTextfuncs;
 ui_globalvars_t* gpGlobals = nullptr;
 
-static const UI_FUNCTIONS gFunctionTable = {
-	nullptr,  // pfnVidInit
-	nullptr,  // pfnInit
-	nullptr,  // pfnShutdown
-	nullptr,  // pfnRedraw
-	nullptr,  // pfnKeyEvent
-	nullptr,  // pfnMouseMove
-	nullptr,  // pfnSetActiveMenu
-	nullptr,  // pfnAddServerToList
-	nullptr,  // pfnGetCursorPos
-	nullptr,  // pfnSetCursorPos
-	nullptr,  // pfnShowCursor
-	nullptr,  // pfnCharEvent
-	nullptr,  // pfnMouseInRect
-	nullptr,  // pfnIsVisible
-	nullptr,  // pfnCreditsActive
-	nullptr,  // pfnFinalCredits
-};
+static int pfnVidInit(void)
+{
+	// TODO
+	return 0;
+}
 
-static UI_EXTENDED_FUNCTIONS gExtendedTable = {
-	nullptr,  // pfnAddTouchButtonToList
-	nullptr,  // pfnResetPing
-	nullptr,  // pfnShowConnectionWarning
-	nullptr,  // pfnShowUpdateDialog
-	nullptr,  // pfnShowMessageBox
-	nullptr,  // pfnConnectionProgress_Disconnect
-	nullptr,  // pfnConnectionProgress_Download
-	nullptr,  // pfnConnectionProgress_DownloadEnd
-	nullptr,  // pfnConnectionProgress_Precache
-	nullptr,  // pfnConnectionProgress_Connect
-	nullptr,  // pfnConnectionProgress_ChangeLevel
-	nullptr,  // pfnConnectionProgress_ParseServerInfo
+static void pfnInit(void)
+{
+	// TODO
+}
+
+static void pfnShutdown(void)
+{
+	// TODO
+}
+
+static void pfnRedraw(float /* flTime */)
+{
+	// TODO
+}
+
+static void pfnKeyEvent(int /* key */, int /* down */)
+{
+	// TODO
+}
+
+static void pfnMouseMove(int /* x */, int /* y */)
+{
+	// TODO
+}
+
+static void pfnSetActiveMenu(int /* active */)
+{
+	// TODO
+}
+
+static void pfnAddServerToList(struct netadr_s /* adr */, const char* /* info */)
+{
+	// TODO
+}
+
+static void pfnGetCursorPos(int* /* pos_x */, int* /* pos_y */)
+{
+	// TODO
+}
+
+static void pfnSetCursorPos(int /* pos_x */, int /* pos_y */)
+{
+	// TODO
+}
+
+static void pfnShowCursor(int /* show */)
+{
+	// TODO
+}
+
+static void pfnCharEvent(int /* key */)
+{
+	// TODO
+}
+
+static int pfnMouseInRect(void)
+{
+	// TODO
+	return 0;
+}
+
+static int pfnIsVisible(void)
+{
+	// TODO
+	return 0;
+}
+
+static int pfnCreditsActive(void)
+{
+	// TODO
+	return 0;
+}
+
+static void pfnFinalCredits(void)
+{
+	// TODO
+}
+
+static void pfnAddTouchButtonToList(
+	const char* /* name */,
+	const char* /* texture */,
+	const char* /* command */,
+	unsigned char* /* color */,
+	int /* flags */
+)
+{
+	// TODO
+}
+
+static void pfnResetPing(void)
+{
+	// TODO
+}
+
+static void pfnShowConnectionWarning(void)
+{
+	// TODO
+}
+
+static void pfnShowUpdateDialog(int /* preferStore */)
+{
+	// TODO
+}
+
+static void pfnShowMessageBox(const char* /* text */)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_Disconnect(void)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_Download(
+	const char* /* pszFileName */,
+	const char* /* pszServerName */,
+	int /* iCurrent */,
+	int /* iTotal */,
+	const char* /* comment */
+)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_DownloadEnd(void)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_Precache(void)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_Connect(const char* /* server */)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_ChangeLevel(void)
+{
+	// TODO
+}
+
+static void pfnConnectionProgress_ParseServerInfo(const char* /* server */)
+{
+	// TODO
+}
+
+static const UI_FUNCTIONS gFunctionTable = {
+	pfnVidInit,
+	pfnInit,
+	pfnShutdown,
+	pfnRedraw,
+	pfnKeyEvent,
+	pfnMouseMove,
+	pfnSetActiveMenu,
+	pfnAddServerToList,
+	pfnGetCursorPos,
+	pfnSetCursorPos,
+	pfnShowCursor,
+	pfnCharEvent,
+	pfnMouseInRect,
+	pfnIsVisible,
+	pfnCreditsActive,
+	pfnFinalCredits
 };
 
 extern "C" EXPORT int
@@ -55,6 +196,21 @@ GetMenuAPI(UI_FUNCTIONS* pFunctionTable, ui_enginefuncs_t* pEngfuncsFromEngine, 
 
 	return 1;
 }
+
+static UI_EXTENDED_FUNCTIONS gExtendedTable = {
+	pfnAddTouchButtonToList,
+	pfnResetPing,
+	pfnShowConnectionWarning,
+	pfnShowUpdateDialog,
+	pfnShowMessageBox,
+	pfnConnectionProgress_Disconnect,
+	pfnConnectionProgress_Download,
+	pfnConnectionProgress_DownloadEnd,
+	pfnConnectionProgress_Precache,
+	pfnConnectionProgress_Connect,
+	pfnConnectionProgress_ChangeLevel,
+	pfnConnectionProgress_ParseServerInfo
+};
 
 extern "C" EXPORT int
 GetExtAPI(int version, UI_EXTENDED_FUNCTIONS* pFunctionTable, ui_extendedfuncs_t* pEngfuncsFromEngine)
