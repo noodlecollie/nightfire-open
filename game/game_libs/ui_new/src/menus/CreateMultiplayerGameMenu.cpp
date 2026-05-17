5a58169bbf99fae94ef54201abee7553825679bc
NoodleCollie <3692034+noodlecollie@users.noreply.github.com>
2026-05-17 10:14:18 +0100

    Some progress towards validated cvars

--- game/game_libs/ui_new/src/menus/CreateMultiplayerGameMenu.cpp
+++ game/game_libs/ui_new/src/menus/CreateMultiplayerGameMenu.cpp
@@ -1,11 +1,13 @@
 #include "menus/CreateMultiplayerGameMenu.h"
+#include <RmlUi/Core/ElementDocument.h>
 
 static constexpr const char* const EVENT_CREATE_GAME = "createGame";
 
 CreateMultiplayerGameMenu::CreateMultiplayerGameMenu() :
 	MenuPage("create_multiplayer_game_menu", "resource/rml/create_multiplayer_game_menu.rml"),
 	m_MenuFrameDataBinding(this),
-	m_PageModel(this)
+	m_PageModel(this),
+	m_InputFieldEventListener(this, &CreateMultiplayerGameMenu::ProcessInputFieldEvent)
 {
 }
 
@@ -22,6 +24,43 @@ bool CreateMultiplayerGameMenu::OnSetUpDataModelBindings(Rml::DataModelConstruct
 	}
 
 	return true;
+}
+
+void CreateMultiplayerGameMenu::OnEndDocumentLoaded()
+{
+	MenuPage::OnEndDocumentLoaded();
+
+	Rml::ElementList elements;
+	Document()->QuerySelectorAll(elements, "input[type='text'][validated-value]");
+
+	for ( Rml::Element* element : elements )
+	{
+		element->AddEventListener(Rml::EventId::Change, &m_InputFieldEventListener);
+		element->AddEventListener(Rml::EventId::Submit, &m_InputFieldEventListener);
+		element->AddEventListener(Rml::EventId::Textinput, &m_InputFieldEventListener);
+	}
+}
+
+void CreateMultiplayerGameMenu::OnBeginDocumentUnloaded()
+{
+	Rml::ElementList elements;
+	Document()->QuerySelectorAll(elements, "input[type='text']");
+
+	for ( Rml::Element* element : elements )
+	{
+		element->RemoveEventListener(Rml::EventId::Change, &m_InputFieldEventListener);
+		element->RemoveEventListener(Rml::EventId::Submit, &m_InputFieldEventListener);
+		element->RemoveEventListener(Rml::EventId::Textinput, &m_InputFieldEventListener);
+	}
+
+	MenuPage::OnBeginDocumentUnloaded();
+}
+
+void CreateMultiplayerGameMenu::ProcessInputFieldEvent(Rml::Event& event)
+{
+	// TODO: It seems we get a textinput event before a new character is added, and
+	// a change event after any change (addition or removal)
+	Rml::Log::Message(Rml::Log::Type::LT_WARNING, "Received event: %s", event.GetType().c_str());
 }
 
 void CreateMultiplayerGameMenu::HandleCreateGame(Rml::DataModelHandle, Rml::Event&, const Rml::VariantList&)
--- game/game_libs/ui_new/src/menus/CreateMultiplayerGameMenu.h
+++ game/game_libs/ui_new/src/menus/CreateMultiplayerGameMenu.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "framework/MenuPage.h"
+#include "framework/EventListenerObject.h"
 #include "templatebindings/MenuFrameDataBinding.h"
 #include "models/CreateMultiplayerGamePageModel.h"
 
@@ -12,9 +13,14 @@ public:
 protected:
 	bool OnSetUpDataModelBindings(Rml::DataModelConstructor& constructor) override;
 
+	void OnEndDocumentLoaded() override;
+	void OnBeginDocumentUnloaded() override;
+
 private:
+	void ProcessInputFieldEvent(Rml::Event& event);
 	void HandleCreateGame(Rml::DataModelHandle, Rml::Event&, const Rml::VariantList&);
 
 	MenuFrameDataBinding m_MenuFrameDataBinding;
 	CreateMultiplayerGamePageModel m_PageModel;
+	EventListenerObject m_InputFieldEventListener;
 };
