c396db44ffbbe814d8aa324e2e842313adf4620a
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-06-22 15:49:06 +0100

    Allowed customising the menu frame

--- game/game_libs/ui_new/src/menus/StyleGuide.cpp
+++ game/game_libs/ui_new/src/menus/StyleGuide.cpp
@@ -1,6 +1,14 @@
 #include "menus/StyleGuide.h"
 
 StyleGuide::StyleGuide() :
-	MenuPage("style_guide", "resource/rml/style_guide.rml")
+	MenuPage("style_guide", "resource/rml/style_guide.rml"),
+	m_MenuFrameDataBinding(this)
 {
+}
+
+void StyleGuide::OnDocumentLoaded()
+{
+	m_MenuFrameDataBinding.SetDefaultTooltipText("None");
+	m_MenuFrameDataBinding.SetTooltipInnerRml("Colour constant: {{footerTooltip}}");
+	m_MenuFrameDataBinding.SetTitle("Style Guide");
 }
