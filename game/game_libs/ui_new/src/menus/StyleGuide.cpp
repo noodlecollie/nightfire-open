84d19322e3efff62c539c7031008177a2b41046e
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-06-25 07:25:44 +0100

    Added colour codes to swatches

--- game/game_libs/ui_new/src/menus/StyleGuide.cpp
+++ game/game_libs/ui_new/src/menus/StyleGuide.cpp
@@ -1,4 +1,7 @@
 #include "menus/StyleGuide.h"
+#include "framework/ElementFinder.h"
+#include <RmlUi/Core/ElementDocument.h>
+#include <RmlUi/Core/StringUtilities.h>
 
 StyleGuide::StyleGuide() :
 	MenuPage("style_guide", "resource/rml/style_guide.rml"),
@@ -11,4 +14,23 @@ void StyleGuide::OnDocumentLoaded()
 	m_MenuFrameDataBinding.SetDefaultTooltipText("None");
 	m_MenuFrameDataBinding.SetTooltipInnerRml("Colour constant: {{footerTooltip}}");
 	m_MenuFrameDataBinding.SetTitle("Style Guide");
+
+	ElementFinder finder;
+
+	Rml::Element* table = Document()->GetElementById("colours_table");
+	Rml::ElementList swatches;
+	finder.AddMulti(&table, ".palette-swatch", &swatches);
+	finder.FindAll();
+
+	for ( Rml::Element* swatch : swatches )
+	{
+		const Rml::Property* bgProp = swatch->GetProperty(Rml::PropertyId::BackgroundColor);
+
+		if ( !bgProp )
+		{
+			continue;
+		}
+
+		swatch->SetInnerRML(Rml::StringUtilities::EncodeRml(bgProp->ToString()));
+	}
 }
