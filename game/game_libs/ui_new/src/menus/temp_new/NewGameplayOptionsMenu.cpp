e48898c751b029e22b9052cc0b16bc15fea19efb
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-09-18 18:24:37 +0100

    Added vibration length option

--- game/game_libs/ui_new/src/menus/temp_new/NewGameplayOptionsMenu.cpp
+++ game/game_libs/ui_new/src/menus/temp_new/NewGameplayOptionsMenu.cpp
@@ -5,7 +5,8 @@
 static constexpr const char* const NAME_CROSSHAIR_ENABLED = "crosshairEnabled";
 static constexpr const char* const NAME_AUTOAIM_ENABLED = "autoaimEnabled";
 static constexpr const char* const NAME_VIBRATION_ENABLED = "vibrationEnabled";
-static constexpr const char* const NAME_VIBRATION_INTENSITY = "vibrationIntensity";
+static constexpr const char* const NAME_VIBRATION_LENGTH = "vibrationLength";
+static constexpr const char* const EVENT_TEST_VIBRATION = "testVibration";
 
 NewGameplayOptionsMenu::NewGameplayOptionsMenu() :
 	NewBaseOptionsMenu("new_gameplay_options_menu", "gameplay_options_menu.rml"),
@@ -14,14 +15,30 @@ NewGameplayOptionsMenu::NewGameplayOptionsMenu() :
 {
 	m_CvarModel.AddEntry<bool>(NAME_CROSSHAIR_ENABLED, "crosshair");
 	m_CvarModel.AddEntry<bool>(NAME_AUTOAIM_ENABLED, "sv_aim");
-	m_CvarModel.AddEntry<bool>(NAME_VIBRATION_ENABLED, "vibration_enable");
-	m_CvarModel.AddEntry<float>(NAME_VIBRATION_INTENSITY, "vibration_length");
+	m_VibrationEnabled = m_CvarModel.AddEntry<bool>(NAME_VIBRATION_ENABLED, "vibration_enable");
+	m_CvarModel.AddEntry<float>(NAME_VIBRATION_LENGTH, "vibration_length");
+}
 
-	m_CvarModel.SetChangeListener(
-		NAME_VIBRATION_INTENSITY,
-		[](const Rml::Variant& newValue)
-		{
-			gEngfuncs.pfnClientCmd(false, Rml::CreateString("vibrate %f", newValue.Get<float>()).c_str());
-		}
-	);
+bool NewGameplayOptionsMenu::OnSetUpDataModelBindings(Rml::DataModelConstructor& constructor)
+{
+	if ( !NewBaseOptionsMenu::OnSetUpDataModelBindings(constructor) )
+	{
+		return false;
+	}
+
+	if ( !constructor.BindEventCallback(EVENT_TEST_VIBRATION, &NewGameplayOptionsMenu::HandleTestVibration, this) )
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void NewGameplayOptionsMenu::HandleTestVibration(Rml::DataModelHandle, Rml::Event&, const Rml::VariantList&)
+{
+	if ( m_VibrationEnabled->CachedValue() )
+	{
+		// The "1" is so that this lasts 1x the value of vibration_length.
+		gEngfuncs.pfnClientCmd(false, "vibrate 1");
+	}
 }
--- game/game_libs/ui_new/src/menus/temp_new/NewGameplayOptionsMenu.h
+++ game/game_libs/ui_new/src/menus/temp_new/NewGameplayOptionsMenu.h
@@ -9,7 +9,13 @@ class NewGameplayOptionsMenu : public NewBaseOptionsMenu
 public:
 	NewGameplayOptionsMenu();
 
+protected:
+	bool OnSetUpDataModelBindings(Rml::DataModelConstructor& constructor) override;
+
 private:
+	void HandleTestVibration(Rml::DataModelHandle, Rml::Event&, const Rml::VariantList&);
+
 	CvarModel m_CvarModel;
 	TooltipComponent m_TooltipComponent;
+	CvarDataVar<bool>* m_VibrationEnabled = nullptr;
 };
